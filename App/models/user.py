fe73816011414dd23e12ea1bd3bd1798894caf13
KCarlton <37227478+KCarlton96@users.noreply.github.com>
2020-11-21 20:53:38 -0400

    Update user.py

--- App/models/user.py
+++ App/models/user.py
@@ -1,11 +1,23 @@
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
+from . import db
 import datetime
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    uwi_id =  db.Column(db.String, nullable=False)
+    uwi_id = db.Column(db.Integer(), unique=True)
+    first_name = db.Column(db.String(50), nullable=False) 
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(120))
+
+    def createUser():
+        return ""
+
+    def checkPassword():
+        return ""
+
+    def changePassword():
+        return ""
     
     def toDict(self):
         return{
