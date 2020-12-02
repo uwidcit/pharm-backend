41a0feb25f6d6471b1aeeceb2bb7297794e0e9fa
KCarlton96 <raquellecarlton96@gmail.com>
2020-12-02 23:53:27 +0000

    product model changed to match sample input

--- App/controllers/product.py
+++ App/controllers/product.py
@@ -3,8 +3,8 @@ from flask import redirect, render_template, request, session, url_for
 from App.models import Product
 from App.models.database import *
 
-def create_product(name, desc, img, price):
-    newProd = Product(name = name, description = desc, image = img, unit_price = price)
+def create_product(code,name, supplier, supplier_price, qoh, stock, unit_price, total):
+    newProd = Product(code = code, product_name = name,  supplier_cost_price = supplier_price, supplier = supplier, QoH = qoh, stock_unit = stock, unit_retail_price = unit_price, total_retail_price = total)
     db.session.add(newProd)
     db.session.commit()
     print("Successfully added")
@@ -18,5 +18,5 @@ def get_all_products():
     return list_of_products
 
 def get_product_by_name(name):
-    product = Product.query.filter_by(name = name).first()
+    product = Product.query.filter_by(product_name = name).first()
     return product.toDict()
--- App/models/product.py
+++ App/models/product.py
@@ -2,24 +2,26 @@ from .database import db
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False)
-    description = db.Column(db.String(500), nullable=False)
-    image = db.Column(db.String(300))
-    unit_price = db.Column(db.Integer, nullable=False)
-
-    def setDescription(self, desc):
-        self.description = desc
-
-    def setImage(self, img):
-        self.image = img
+    code = db.Column(db.Float, nullable=False)
+    product_name = db.Column(db.String(300), nullable=False)    
+    supplier_cost_price = db.Column(db.Float, nullable=False)
+    supplier = db.Column(db.String(200), nullable = False)    
+    QoH = db.Column(db.String(200), nullable = False)
+    stock_unit = db.Column(db.Float, nullable = False)
+    unit_retail_price = db.Column(db.Float, nullable = False)
+    total_retail_price = db.Column(db.Float, nullable=False)
     
     def setPrice(self, price):
-        self.unit_price = price
+        self.unit_retail_price = price
 
     def toDict(self):
         return {
-            'name': self.name,
-            'description': self.description,
-            'image': self.image,
-            'unit_price': self.unit_price
+            "code": self.code,
+            "product_name": self.product_name,
+            "supplier": self.supplier,
+            "supplier_cost_price": self.supplier_cost_price,
+            "QoH": self.QoH,
+            "stock_unit": self.stock_unit,
+            "unit_retail_price": self.unit_retail_price,
+            "total_retail_price": self.total_retail_price
         }
