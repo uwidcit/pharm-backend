2abe63d1adb08a4540daf40ad5989d1b71b0d51a
Nicholas Mendez <snickdx@gmail.com>
2020-12-05 05:53:34 +0000

    updated config

--- App/config.py
+++ App/config.py
@@ -1,12 +1,22 @@
 import json, os
 
+CONFIG = None
 
 environment = os.environ.get('ENV')
 if environment is None:
     environment = 'staging'
-
-CONFIG = None
-
-with open('environment.'+environment+'.json') as config_file:
-    CONFIG = json.load(config_file)
-    CONFIG['ENV']=environment
+    with open('environment.'+environment+'.json') as config_file:
+        CONFIG = json.load(config_file)
+else:
+    CONFIG = {
+        "dbhost" : os.environ.get('DBHOST'),
+        "debug" : True,
+        "JWTdeltaDays": 7,
+        "uploadDir":"App/uploads/",
+        "dbname" : os.environ.get('DBNAME'),
+        "dbuser" : os.environ.get('DBUSER'),
+        "secret_key" : os.environ.get('SECRET_KEY'),
+        "dbpassword": os.environ.get('DBPASSWORD'),
+        "dbport" : 3306
+    }
+CONFIG['ENV']=environment
